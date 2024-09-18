a050b3d673dab035088e8d177fdc5ae98196e156
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-09-18 22:53:35 +0600

    Handle Login and redirect to the home page

--- js/login.js
+++ js/login.js
@@ -1,8 +1,32 @@
+// document.getElementById('button-login').addEventListener('click',function(event){
+//     event.preventDefault();
+//     console.log("LogIn Button Click");
+
+
+//     const phoneNumber = document.getElementById('phone-number').value;
+//     const pinNumber = document.getElementById('pin-number').value;
+//     console.log(phoneNumber,pinNumber)
+
+//     if(phoneNumber == '017' && pinNumber === '1234'){
+//         console.log('Your are logged in');
+//     }else{
+//         console.log('Invalid phone number or pin number');
+//     }
+// })
+
+
 document.getElementById('button-login').addEventListener('click',function(event){
     event.preventDefault();
-    console.log("LogIn Button Click");
 
+    // Get Phone number and pin
 
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber)
+    const pinNumber = document.getElementById('pin-number').value;
+    if(phoneNumber == '017' && pinNumber === '1234'){
+        console.log('Your are logged in');
+        window.location.href= "/home.html"
+    }else{
+        alert('Invalid phone number or pin number');
+    }
+
 })
