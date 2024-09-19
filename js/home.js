7a9870d9287a611260ca0697018cfd724309ea67
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-09-19 11:14:30 +0600

    Verify Pin Number and add money to the balance

--- js/home.js
+++ js/home.js
@@ -8,4 +8,19 @@ document.getElementById('btn-add-money')
 
     const pinNumberInput = document.getElementById('input-pin-number').value;
     console.log(pinNumberInput);
+
+    if(pinNumberInput === '1234'){
+        console.log('adding money to your account')
+        const balance = document.getElementById('account-balance').innerText;
+        console.log(balance)
+
+        const addMoneyNumber =parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = balanceNumber + addMoneyNumber;
+        console.log(newBalance);
+        document.getElementById('account-balance').innerText = newBalance;
+    }else{
+        alert('Failed to add money! Please Try Again')
+    }
+
 });
